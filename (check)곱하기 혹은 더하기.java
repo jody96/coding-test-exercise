ad982d9e1e7043fc72fb310326dce9e9103c0989
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2021-09-06 10:29:47 +0900

    Create (check)곱하기 혹은 더하기.java

---
+++ (check)곱하기 혹은 더하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    String s = sc.next();
+
+    long result = str.charAt(0) - '0';
+
+    for(int i=1; i<str.length(); i++){
+      int num = str.charAt(i) - '0';
+      if(num <= 1 || result <= 1){
+        result += num;
+      } else{
+        result *= num;
+      }
+    }
+    System.out.println(result);
+  }
+}
