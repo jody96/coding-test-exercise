f5e5460240c6a8077748fb09dce62ad2a9bf9c1a
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2021-09-07 10:28:52 +0900

    Create (check)만들 수 없는 금액.java

---
+++ (check)만들 수 없는 금액.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main {
+  public static int n;
+  public static ArrayList<Integer> arrayList = new ArrayList<>();
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    
+    for(int i=0; i<n; i++){
+      arrayList.add(sc.nextInt());
+    }
+
+    Collections.sort(arrayList);
+
+    int target = 1;
+    for(int i=0; i<n; i++){
+      if(target < arrayList.get(i)) break;
+      target += arrayList.get(i);
+    }
+
+    System.out.println(target);
+  }
+  
+}
