c9d007dd0af095bf5bd284640865026bb8320ae1
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2021-09-22 09:17:37 +0900

    Update (check)18405.java

--- (check)18405.java
+++ (check)18405.java
@@ -1,91 +1,75 @@
 import java.util.*;
 
 class Virus implements Comparable<Virus>{
-  private int index;
+  private int kind;
   private int second;
   private int x;
   private int y;
 
-  public Virus(int index, int second, int x, int y){
-    this.index = index;
-    this.second = second;
+  Virus(int kind, int second, int x, int y){
+    this.kind = kind;
+    this.second  = second;
     this.x = x;
     this.y = y;
   }
 
-  public int getIndex(){
-    return this.index;
+  public int getKind(){
+    return kind;
   }
-
   public int getSecond(){
-    return this.second;
+    return second;
   }
-
   public int getX(){
-    return this.x;
+    return x;
   }
-
   public int getY(){
-    return this.y;
+    return y;
   }
 
   @Override
   public int compareTo(Virus other){
-    if(this.index < other.index){
-      return -1;
-    }
-    return 1;
+    if(this.second == other.second)
+      return this.kind - other.kind;
+    return this.second - other.second;
   }
 }
 
 class Main {
-  public static int n, k;
+  public static int n, k, s, x, y;
   public static int[][] graph = new int[200][200];
-  public static ArrayList<Virus> viruses = new ArrayList<Virus>();
-
-  public static int[] dx = {-1,0,1,0};
-  public static int[] dy = {0,1,0,-1};
-
+  public static int[][] vector = {{-1,0},{0,1},{1,0},{0,-1}};
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
     n = sc.nextInt();
     k = sc.nextInt();
-
+    PriorityQueue<Virus> queue = new PriorityQueue<>();
     for(int i=0; i<n; i++){
       for(int j=0; j<n; j++){
         graph[i][j] = sc.nextInt();
-        if(graph[i][j] != 0){
-          viruses.add(new Virus(graph[i][j], 0, i, j));
-        }
+        if(graph[i][j] != 0)
+          queue.add(new Virus(graph[i][j], 0, i, j));
       }
     }
+    s = sc.nextInt();
+    x = sc.nextInt();
+    y = sc.nextInt();
 
-    Collections.sort(viruses);
-    Queue<Virus> q = new LinkedList<Virus>();
-    for(int i=0; i<viruses.size(); i++){
-      q.offer(viruses.get(i));
-    }
-
-    int targetS = sc.nextInt();
-    int targetX = sc.nextInt();
-    int targetY = sc.nextInt();
-
-    while(!q.isEmpty()){
-      Virus virus = q.poll();
-      if(virus.getSecond() == targetS) break;
+    while(!queue.isEmpty()){
+      Virus now = queue.poll();
+      if(now.getSecond() == s)
+        continue;
       for(int i=0; i<4; i++){
-        int nx = virus.getX() + dx[i];
-        int ny = virus.getY() + dy[i];
-        if(0 <= nx && nx < n && 0 <= ny && ny < n){
-          if(graph[nx][ny] == 0){
-            graph[nx][ny] = virus.getIndex();
-            q.offer(new Virus(virus.getIndex(), virus.getSecond() + 1, nx, ny));
-          }
+        int nx = now.getX() + vector[i][0];
+        int ny = now.getY() + vector[i][1];
+        if(nx >= n || nx < 0 || ny >= n || ny < 0)
+          continue;
+        if(graph[nx][ny] == 0){
+          graph[nx][ny] = now.getKind();
+          queue.add(new Virus(now.getKind(), now.getSecond()+1, nx, ny));
         }
       }
     }
-
-    System.out.println(graph[targetX - 1][targetY - 1]);
+    
+    System.out.println(graph[x-1][y-1]);
   }
 }
