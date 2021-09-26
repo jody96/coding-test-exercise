71a19c272820f3663e2c470344598bf6e5d9ba1c
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2021-09-26 10:51:55 +0900

    Create (check)1715.java

---
+++ (check)1715.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main {
+  public static int n, result;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    for(int i=0; i<n; i++){
+      pq.offer(sc.nextInt());
+    }
+
+    while(pq.size() != 1){
+      int one = pq.poll();
+      int two = pq.poll();
+      int summary = one + two;
+      result += summary;
+      pq.offer(summary);    
+    }
+    System.out.println(result);
+  }
+}
